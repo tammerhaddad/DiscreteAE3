206eee3ab3dc0a3ad8f1bb9a8c53f94230ac9586
seanma711 <sean.ma711@gmail.com>
2023-11-29 18:52:19 +0000

    small changes

--- test/test.py
+++ test/test.py
@@ -8,12 +8,15 @@ start = time.time()
 deck = Deck()
 numPlayers = 2
 hands = [deck.draw(2) for _ in range(numPlayers)]
-unknown = [card for card in deck.cards if card not in hands[0]]
+table = deck.draw(5)
+player1hand = hands[0] + table
+unknown = [card for card in deck.cards if card not in player1hand]
 
-possibleHands = list(itertools.combinations(unknown, 5))
+opposingHands = list(itertools.combinations(unknown, 5))
+player1Hands = [Hand(hand) for hand in list(itertools.combinations(player1hand, 5))]
 
 # makeThemHands = [Hand(hand) for hand in possibleHands]
 # makeThemHands.sort()
-for a in unknown:
-    print(a)
-print(f"{len(unknown)}, Time: {time.time()-start:.2f}")
+# for a in unknown:
+#     print(a)
+# print(f"{len(unknown)}, Time: {time.time()-start:.2f}")
