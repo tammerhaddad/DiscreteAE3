f2c14eb844d5882551ba41443a091e8950710cbc
tammerrh <tammerrh@gmail.com>
2023-11-30 13:59:40 +0000

    probs

--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@ import itertools
 import time
 start = time.time()
 fullTime = time.time()
-output = False
+output = True
 
 def stringToHand(str):
     return Hand([Card(int(a.split("-")[0]), a.split("-")[1]) for a in str.split(",")])
@@ -19,6 +19,7 @@ def ptime(prefix):
 
 allHands = open('test/sorted.txt').read().splitlines()
 ptime("Read")
+length = len(allHands)
 numPlayers = 2
 
 def calc():
@@ -26,14 +27,14 @@ def calc():
     players = [deck.draw(2) for _ in range(numPlayers)]
     table = deck.draw(5)
     comb = table + players[0]
+    unknown = deck.cards + [card for player in players[1:] for card in player]
     ptime("Initialize")
+    oppHands = [Hand(hand) for hand in itertools.combinations(unknown, 2)]
     combs = [Hand(hand) for hand in itertools.combinations(comb, 5)]
     ptime("Hand Possibilities")
     best = max(combs)
-    print(f"{best}, {best.strRank()}")
+    print(f"Table: {list(map(str, table))}\nHand: {list(map(str, players[0]))}\nBest Hand: {best}, {best.strRank()}")
     return allHands.index(str(best))
-# print(f"Table: {list(map(str, table))}\nHand: {list(map(str, players[0]))}\nBest Hand: {best}, {best.strRank()}\nProb = {best_index/len(allHands)}")
 # print(f"Total Time: {time.time()-fullTime} seconds.")
 
-for i in range(10):
-    print(f"Prob = {calc()/len(allHands)}")
+print(f"Prob = {calc()/length}")
