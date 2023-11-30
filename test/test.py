29473934264d36c373d9a672c36556759011e099
tammerrh <tammerrh@gmail.com>
2023-11-30 14:12:36 +0000

    update

--- test/test.py
+++ test/test.py
@@ -5,26 +5,36 @@ import itertools
 import time
 start = time.time()
 fullTime = time.time()
+output = True
+
 def stringToHand(str):
     return Hand([Card(int(a.split("-")[0]), a.split("-")[1]) for a in str.split(",")])
 
 def ptime(prefix):
     global start
-    print(f"{prefix} Time: {time.time()-start:.2f}s")
-    start = time.time()
+    global output
+    if(output):
+        print(f"{prefix} Time: {time.time()-start:.2f}s")
+        start = time.time()
 
-deck = Deck()
-numPlayers = 2
-players = [deck.draw(2) for _ in range(numPlayers)]
-table = deck.draw(5)
-comb = table + players[0]
-ptime("Initialize")
 allHands = open('test/sorted.txt').read().splitlines()
 ptime("Read")
-combs = [Hand(hand) for hand in itertools.combinations(comb, 5)]
-ptime("Hand Possibilities")
-best = max(combs)
-print(f"{best}, {best.strRank()}")
-best_index = allHands.index(str(best))
-print(f"Table: {list(map(str, table))}\nHand: {list(map(str, players[0]))}\nBest Hand: {best}, {best.strRank()}\nProb = {best_index/len(allHands)}")
-print(f"Total Time: {time.time()-fullTime} seconds.")
+length = len(allHands)
+numPlayers = 2
+
+def calc(players, deck, table):
+    known = table + players[0]
+    unknown = deck.cards + [card for player in players[1:] for card in player]
+    ptime("Initialize")
+    oppHands = [Hand(hand) for hand in itertools.combinations(unknown, 2)]
+    combs = [Hand(hand) for hand in itertools.combinations(known, 5)]
+    ptime("Hand Possibilities")
+    best = max(combs)
+    print(f"Table: {list(map(str, table))}\nHand: {list(map(str, players[0]))}\nBest Hand: {best}, {best.strRank()}")
+    return allHands.index(str(best))
+# print(f"Total Time: {time.time()-fullTime} seconds.")
+
+deck = Deck()
+players = [deck.draw(2) for _ in range(numPlayers)]
+table = deck.draw(5)
+print(f"Prob = {calc(players, deck, table)/length}")
