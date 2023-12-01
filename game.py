e4fb03ec1d8db9fd5d85bdfecfd3e8887aaa0a1f
seanma711 <sean.ma711@gmail.com>
2023-12-01 13:38:17 +0000

    fixed crashing issue at end of game

--- game.py
+++ game.py
@@ -1,9 +1,13 @@
 from poker import Poker
 game = Poker(2)
 
-for i in range(4):
-    game.renderGame()
-    while game.gameUpdate():
-        game.pause()
-    game.dealTable(i)
-    game.updateProbs()
+def play():
+    for i in range(4):
+        game.renderGame()
+        while game.gameUpdate():
+            game.pause()
+        game.dealTable(i)
+        if i == 3:
+            break
+        game.updateProbs()
+
