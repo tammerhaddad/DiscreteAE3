95eac724e4bf50c8c46da01c0a59e7fb4847f822
tammerrh <tammerrh@gmail.com>
2023-12-01 11:39:30 +0000

    working postFlopProb

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from deck import Deck
 import itertools
 import time
 import pickle
+from bisect import bisect
 
 start = time.time()
 fullTime = time.time()
@@ -21,7 +22,7 @@ ptime("Read")
 setHands = set(allHands)
 ptime("toSet")
 length = len(allHands)
-numPlayers = 2
+numPlayers = 3
 blankDeck = Deck()
 
 #------------------------------------------------------------
@@ -30,39 +31,64 @@ def play():
     deck = Deck()
     players = [deck.draw(2) for _ in range(numPlayers)]
     table = []
+    probs = [1/numPlayers]*numPlayers
     for i in range(4):
+        stepTime = time.time()
         # input("enter for next")
         if i == 1:
             table += deck.draw(3)
-            print(f"Prob: {postFlop(players, table)}")
+            probs = postFlop(players, table, deck)
         elif i > 1:
             table += deck.draw()
-            print(f"Prob: {postFlop(players, table)}")
+            probs = postFlop(players, table, deck)
+        else:
+            # probs = preFlop(players)
+            # probs = postFlop(players, table, deck)
+            pass
+        print(f"Prob: {probs} SUM: {sum(probs)}")
+        if output: print(f"STEPTIME: {time.time()-stepTime}")
     print(f"Table: {list(map(str, table))}\nHands: {[f'{list(map(str, player))}' for player in players]}")
 
+# gets the best hand given player and a table (the table must have at least 3 cards in it)
 def bestHand(player, table):
     return max([Hand(hand) for hand in itertools.combinations(player+table, 5)])
 
-#------------------------------------------------------------------
+# returns the index in players of the player that won
+def winner(players, table):
+    bHands = [bestHand(player, table) for player in players]
+    return bHands.index(max(bHands))
 
-def postFlop(players, table):
-    unknown = set(blankDeck.cards) - set(players[0]) - set(table)
-    tables = [table+list(hand) for hand in itertools.combinations(unknown, 5-len(table))]
-    myHands = [bestHand(players[0], ptable) for ptable in tables]
-    oppHands = [bestHand(players[1], ptable) for ptable in tables]
-    return calculate_probability(myHands, oppHands)
-
-def calculate_probability(list_a, list_b):
-    total_pairs = len(list_a) * len(list_b)
-    a_beats_b = sum(1 for item_a, item_b in itertools.product(list_a, list_b) if item_a > item_b)
-    probability_a_beats_b = a_beats_b / total_pairs
-    probability_b_beats_a = 1 - probability_a_beats_b
-
-    return probability_a_beats_b, probability_b_beats_a
 
 #------------------------------------------------------------------
-for _ in range(5):
-    ptime("Before")
+# FAILED PREFLOP CALCULATOR
+
+def preFlop(players):
+    playerHands = [set(filter(lambda hand: any(card in hand.hand for card in player), setHands)) for player in players]
+    pTables = setHands - set.union(*playerHands)
+    print(len(pTables))
+    return [len(hands) for hands in playerHands]
+
+
+def preFlopProb(a, b):
+    wins = 0
+    for hand_a in a:
+        wins += bisect(b, hand_a)
+    return wins / (len(a) * len(b))
+
+#----------------------------------------------------------
+# Prob Calculator (can be used for preflop but would take 1439 times longer than post flop, 20 minutes on my computer)
+
+def postFlop(players, table, deck):
+    unknown = set(deck.cards)
+    ptables = [table+list(adds) for adds in itertools.combinations(unknown, 5-len(table))]
+    # pHands = [[bestHand(player, ptable) for ptable in tables] for player in players]
+    print(len(ptables))
+    ptime("ptables")
+    winners = [winner(players, ptable) for ptable in ptables]
+    return [winners.count(i)/len(winners) for i in range(len(players))]
+
+#------------------------------------------------------------------
+for _ in range(3):
+    t = time.time()
     play()
-    ptime("After")
-    print("--------------------------------------")
+    print(f"{time.time()-t:.2f}s ----------------------------")
